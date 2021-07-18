fb86d1e6fd692a033bee475ad229d18780688ffb
dbstjrwnekd <sh4869@ajou.ac.kr>
2021-07-18 19:55:52 +0900

    update-delete-message

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -10,15 +10,17 @@ const originalMsgs = Array(50)
   .map((_, i) => ({
     id: 50 - i,
     userId: getRandomUserId(),
-    timeStamp: 1234567890123 + i * 1000 * 60,
-    text: `${i + 1} mock text`,
+    timeStamp: 1234567890123 + (50 - i) * 1000 * 60,
+    text: `${50 - i} mock text`,
   }));
 
 const MsgList = () => {
   const [msgs, setMsgs] = useState(originalMsgs);
+  const [editingId, setEditingId] = useState(null);
+
   const onCreateMessage = (text) => {
     const newMsg = {
-      id: msgs.length,
+      id: msgs.length + 1,
       userId: getRandomUserId(),
       timeStamp: Date.now(),
       text: `${msgs.length + 1} ${text}`,
@@ -26,12 +28,45 @@ const MsgList = () => {
     setMsgs((msgs) => [newMsg, ...msgs]);
   };
 
+  const onUpdateMessage = (text, id) => {
+    setMsgs((msgs) => {
+      const targetIndex = msgs.findIndex((msg) => msg.id === id);
+      if (targetIndex < 0) return msgs;
+      const newMsgs = [...msgs];
+      newMsgs.splice(targetIndex, 1, {
+        ...msgs[targetIndex],
+        text,
+      });
+      return newMsgs;
+    });
+    doneEdit();
+  };
+
+  const doneEdit = () => setEditingId(null);
+
+  const onDeleteMessage = (id) => {
+    setMsgs((msgs) => {
+      const targetIndex = msgs.findIndex((msg) => msg.id === id);
+      if (targetIndex < 0) return msgs;
+      const newMsgs = [...msgs];
+      newMsgs.splice(targetIndex, 1);
+      return newMsgs;
+    });
+  };
+
   return (
     <>
       <MsgInput mutate={onCreateMessage} />
       <ul className="messages">
         {msgs.map((msg) => (
-          <MsgItem key={msg.id} {...msg} />
+          <MsgItem
+            key={msg.id}
+            {...msg}
+            onUpdateMessage={onUpdateMessage}
+            onDeleteMessage={() => onDeleteMessage(msg.id)}
+            startEdit={() => setEditingId(msg.id)}
+            isEditing={editingId === msg.id}
+          />
         ))}
       </ul>
     </>
